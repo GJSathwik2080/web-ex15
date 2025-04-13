0dd8d6cd5f12a8a30a477947209c7df37024a5d4
GJSathwik2080 <grandhi.jagannadha2023@vitstudent.ac.in>
2025-04-14 01:05:21 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+const App = () => (
+<div>
+<Header title="Welcome to My React App" />
+<Content />
+<Footer />
+</div>
+);
+export default App
+
